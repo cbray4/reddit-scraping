89119894cbf630b52b46cd25b196567a13b441fe
Carver Bray <106403722+cbray4@users.noreply.github.com>
2023-12-04 13:35:48 -0700

    Update bashReddit.sh

--- scripts/bashReddit.sh
+++ scripts/bashReddit.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00 
-#SBATCH --mem=5G
+#SBATCH --mem=1G
 #SBATCH --output=/project/redditsa/reddit-scraping/slurmOutput
 #SBATCH --error=/project/redditsa/reddit-scraping/slurmError
 #SBATCH --mail-type=END
